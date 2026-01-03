19a960d07f93044ff55bc2fe42ca3e4ec4227e83
Developer11861 <11861@reports.re>
2026-01-03 13:17:59 +0200

    Update DevicePositionViewController

--- DevicePositionUIKitWithCombine/DevicePositionUIKitWithCombine/DevicePositionViewController.swift
+++ DevicePositionUIKitWithCombine/DevicePositionUIKitWithCombine/DevicePositionViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 struct Constants {
     static let deviceMotionUpdateInterval = 1.0 / 60.0
@@ -16,11 +17,52 @@ struct Constants {
 
 class DevicePositionViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var frontalTiltLabel: UILabel!
+    @IBOutlet weak var lateralTiltLael: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+
+    // MARK: - ViewModel
+
+    lazy var viewModel = DevicePositionViewModel()
+
+    // MARK: - Combine
+
+    var bag = Set<AnyCancellable>()
+
+    // MARK: - Life cycle ViewController
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        viewModel.$deviceFrontalTilt
+            .compactMap { $0?.message }
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.text, on: frontalTiltLabel)
+            .store(in: &bag)
+
+        viewModel.$deviceLateralTilt
+            .compactMap { $0?.message }
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.text, on: lateralTiltLael)
+            .store(in: &bag)
+
+        viewModel.$devicePosition
+            .compactMap { $0?.message }
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.text, on: positionLabel)
+            .store(in: &bag)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.start()
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        viewModel.stop()
+    }
 }
 
