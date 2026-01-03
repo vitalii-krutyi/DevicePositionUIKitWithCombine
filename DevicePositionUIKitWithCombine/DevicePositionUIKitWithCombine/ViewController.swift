90d9b82adac308b23d73a8a4679ab2a4bd34b9e6
Developer11861 <11861@reports.re>
2026-01-03 11:24:10 +0200

    Create new project

---
+++ DevicePositionUIKitWithCombine/DevicePositionUIKitWithCombine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevicePositionUIKitWithCombine
+//
+//  Created by Vitalii Krutyi on 03.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
